3cd2079169d03323bce058389f909a6ba3a28c18
leoteles <leonardo.teles1704@gmail.com>
2025-02-17 15:49:57 -0300

    tarefa mod9

---
+++ tarefaMod9/src/mod9/Exemplo.java
@@ -0,0 +1,14 @@
+package mod9;
+
+public class Exemplo {
+	
+	public static void main(String args[]) {
+	
+		long num1 = 1000000000;
+		System.out.println(num1);
+
+		Long num2 = Long.valueOf(num1);
+		System.out.println(num2);
+	}
+	
+}
