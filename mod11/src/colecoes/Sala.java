a82d41800a1f8d039c3ff6e9ac0f65570f2b7a22
leoteles <leonardo.teles1704@gmail.com>
2025-02-20 15:26:15 -0300

    tarefa mod11

---
+++ mod11/src/colecoes/Sala.java
@@ -0,0 +1,48 @@
+package colecoes;
+
+import colecoes.Aluno;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Sala {
+	
+	 public static void main(String args[]) {
+	        ListaSimplesAlunos();
+	    }
+	 
+	 private static void ListaSimplesAlunos() {
+	        System.out.println("****** ListaSimplesAlunos ******");
+	        List<Aluno> lista = new ArrayList<Aluno>();
+
+	        Aluno a = new Aluno("João Pedro", "Masculino");
+	        Aluno b = new Aluno("Antônia Silva", "Feminino");
+	        Aluno c = new Aluno("Francisco José", "Masculino");
+	        Aluno d = new Aluno("Mariana Andrade", "Feminino");
+	        lista.add(a);
+	        lista.add(b);
+	        lista.add(c);
+	        lista.add(d);
+	        System.out.println(lista);
+	        Collections.sort(lista);
+	        System.out.println(lista);
+	        System.out.println("");
+	        
+	        System.out.println("Gênero Masculino:");
+	        for (Aluno aluno : lista) {
+	            if (aluno.getSexo().startsWith("M")) {
+	                System.out.println(aluno);
+	            }
+	        }
+
+	        System.out.println(" ");
+
+	        System.out.println("Gênero Feminino:");
+	        for (Aluno aluno : lista) {
+	            if (aluno.getSexo().startsWith("F")) {
+	                System.out.println(aluno);
+	            }
+	        }
+	    }
+}
